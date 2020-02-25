6eb752e02d5cedf5b0c6688f7868ba9cbcaf1081
Cameron Tacklind <cameron@tacklind.com>
2020-02-24 17:55:08 -0800

    Force a HOME dir

--- src/prepare/setup/MainServiceFiles.ts
+++ src/prepare/setup/MainServiceFiles.ts
@@ -1,5 +1,5 @@
 import { exec } from '../../utils/spawn';
-import { ensureFileIs, mkdir } from '../../utils/fs';
+import { ensureFileIs, mkdir, chmod } from '../../utils/fs';
 import { readAvenConfig } from '../../utils/readAvenConfig';
 
 export async function setupMainServiceFiles(): Promise<void> {
@@ -10,9 +10,11 @@ export async function setupMainServiceFiles(): Promise<void> {
   const startServerCommand = config.startServerCommand ?? '/usr/bin/npm start';
 
   const serviceFile = `/etc/systemd/system/${serviceName}.service`;
+  const HomeDir = `/var/lib/${serviceName}`;
 
   const dir1 = mkdir(`${serviceFile}.d`);
   const dir2 = mkdir(`/opt/aven`);
+  const dir3 = mkdir(HomeDir).then(() => chmod(HomeDir, 0o700));
 
   const serviceFileContents = `[Unit]
   Description=${serviceDescription}
@@ -24,6 +26,7 @@ export async function setupMainServiceFiles(): Promise<void> {
   RuntimeDirectory=${serviceName}
   WorkingDirectory=/opt/aven/${serviceName}
   ExecStart=${startServerCommand}
+  echo Environment=HOME="${HomeDir}"
   User=www-data
   
   [Install]
@@ -38,5 +41,5 @@ export async function setupMainServiceFiles(): Promise<void> {
   // Run service on server boot
   await exec(`systemctl enable ${serviceName}.service`);
 
-  await Promise.all([dir1, dir2]);
+  await Promise.all([dir1, dir2, dir3]);
 }
